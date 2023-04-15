dbd2d4a4e2badc7ffb5615609fb09fb3b52de20d
leyixu21 <leyixu@outlook.com>
2023-04-15 23:35:59 +0200

    Update the loadCityList to add city attractions

--- src/main/java/ch/uzh/ifi/hase/soprafs23/entity/CityBase.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/entity/CityBase.java
@@ -3,6 +3,7 @@ package ch.uzh.ifi.hase.soprafs23.entity;
 import ch.uzh.ifi.hase.soprafs23.constant.CityCategory;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -25,11 +26,11 @@ public class CityBase {
     private void loadCityList() {
         // for temp. use
         if(category == CityCategory.EUROPE){
-            City paris = new City("Paris", "France", "N48 E2");
-            City berlin = new City("Berlin", "Germany", "N52 E13");
-            City zurich = new City("Zurich", "Switzerland", "N47 E8");
-            City london = new City("London", "UnitedKingdom", "N51 E0");
-            City geneve = new City("Geneve", "Switzerland", "N46 E6");
+            City paris = new City("Paris", "France", "N48 E2", new ArrayList<>(Arrays.asList("Eiffel Tower", "Arc de Triomphe", "Luxembourg Gardens")));
+            City berlin = new City("Berlin", "Germany", "N52 E13", new ArrayList<>(Arrays.asList("Brandenburg Gate", "Tempelhofer Feld", "Altes Museum")));
+            City zurich = new City("Zurich", "Switzerland", "N47 E8", new ArrayList<>(Arrays.asList("Lindenhof", "Grossmünster", "Fraumünster Church")));
+            City london = new City("London", "UnitedKingdom", "N51 E0", new ArrayList<>(Arrays.asList("London Eye", "Tower of London", "Tower Bridge")));
+            City geneve = new City("Geneve", "Switzerland", "N46 E6", new ArrayList<>(Arrays.asList("Geneva Water Fountain", "Palais des Nations", "Brunswick Monument")));
             cityList.add(paris);cityList.add(berlin);cityList.add(london);
             cityList.add(zurich);cityList.add(geneve);
         }
