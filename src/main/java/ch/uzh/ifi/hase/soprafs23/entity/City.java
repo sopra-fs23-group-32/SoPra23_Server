7ef40f91bd424150c82391b42ec61e061651bfc7
leyixu21 <leyixu@outlook.com>
2023-04-15 23:35:27 +0200

    Update the method generatePicture to return city image url

--- src/main/java/ch/uzh/ifi/hase/soprafs23/entity/City.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/entity/City.java
@@ -1,16 +1,24 @@
 package ch.uzh.ifi.hase.soprafs23.entity;
 
-import javax.xml.stream.Location;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class City {
     public String name;
     public String country;
     public String location;
+    private List<String> attractions;
 
-    public City(String name, String country, String location) {
+    public City(String name, String country, String location, List<String> attractions) {
         this.name = name;
         this.country = country;
         this.location = location;
+        this.attractions = attractions;
     }
 
     public String getName() {return name;}
@@ -18,9 +26,16 @@ public class City {
     public String getCountry() {return country;}
 
     public String getLocation() {return location;}
+    public List<String> getAttractions() {return attractions;}
 
     /**
-     * Use flicker API to get picture online
+     * Use flicker API to get the city image url
      */
-    public void generatePicture() {}
+    public URL generatePicture() throws URISyntaxException, IOException {
+        Random rand = new Random(attractions.size());
+        String attraction = attractions.get(rand.nextInt());
+
+        FlickrImageCollector imageCollecter = new FlickrImageCollector(attraction);
+        return imageCollecter.getImageUrl();
+    }
 }
