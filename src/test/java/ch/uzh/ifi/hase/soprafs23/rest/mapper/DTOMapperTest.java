aa52f033ae49940cf9e8680ce4025f660b7bba7b
leyixu21 <leyixu@outlook.com>
2023-04-20 23:40:22 +0200

    Update DTOMapperTest

--- src/test/java/ch/uzh/ifi/hase/soprafs23/rest/mapper/DTOMapperTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/rest/mapper/DTOMapperTest.java
@@ -1,11 +1,11 @@
 package ch.uzh.ifi.hase.soprafs23.rest.mapper;
 
 import java.util.Date;
+
+import ch.uzh.ifi.hase.soprafs23.constant.CityCategory;
 import ch.uzh.ifi.hase.soprafs23.constant.UserStatus;
-import ch.uzh.ifi.hase.soprafs23.entity.User;
-import ch.uzh.ifi.hase.soprafs23.rest.dto.UserGetDTO;
-import ch.uzh.ifi.hase.soprafs23.rest.dto.UserPostDTO;
-import ch.uzh.ifi.hase.soprafs23.rest.dto.UserPutDTO;
+import ch.uzh.ifi.hase.soprafs23.entity.*;
+import ch.uzh.ifi.hase.soprafs23.rest.dto.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -62,5 +62,79 @@ public class DTOMapperTest {
         assertEquals(userPutDTO.getUsername(), user.getUsername());
         assertEquals(userPutDTO.getPassword(), user.getPassword());
         assertEquals(userPutDTO.getBirthDay(), user.getBirthDay());
+    }
+
+    @Test
+    public void testCreateGame_fromGamePostDTO_toGame_success() {
+        // create GamePostDTO
+        GamePostDTO gamePostDTO = new GamePostDTO();
+        gamePostDTO.setGameId(1L);
+        gamePostDTO.setCategory(CityCategory.ASIA);
+        gamePostDTO.setCountdownTime(20);
+        gamePostDTO.setTotalRounds(5);
+        // MAP -> Game
+        Game game = DTOMapper.INSTANCE.convertGamePostDTOtoEntity(gamePostDTO);
+        // check content
+        assertEquals(gamePostDTO.getGameId(), game.getGameId());
+        assertEquals(gamePostDTO.getCategory(), game.getCategory());
+        assertEquals(gamePostDTO.getCountdownTime(), game.getCountdownTime());
+        assertEquals(gamePostDTO.getTotalRounds(), game.getTotalRounds());
+    }
+
+    @Test
+    public void testGetQuestion_fromQuestion_toQuestionGetDTO_success() {
+        // create Question
+        Question question = new Question("A", "B", "C", "D", "A", "url");
+        // MAP -> QuestionGetDTO
+        QuestionGetDTO questionGetDTO = DTOMapper.INSTANCE.convertEntityToQuestionGetDTO(question);
+        // check content
+        assertEquals(question.getOption1(), questionGetDTO.getOption1());
+        assertEquals(question.getOption2(), questionGetDTO.getOption2());
+        assertEquals(question.getOption3(), questionGetDTO.getOption3());
+        assertEquals(question.getOption4(), questionGetDTO.getOption4());
+        assertEquals(question.getCorrectOption(), questionGetDTO.getCorrectOption());
+        assertEquals(question.getPictureUrl(), questionGetDTO.getPictureUrl());
+    }
+
+    @Test
+    public void testCreateAnswer_fromAnswerPostDTO_toAnswer_success() {
+        // create AnswerPostDTO
+        AnswerPostDTO answerPostDTO = new AnswerPostDTO();
+        answerPostDTO.setAnswer("C");
+        answerPostDTO.setTimeTaken(3);
+        // MAP -> Answer
+        Answer answer = DTOMapper.INSTANCE.convertAnswerPostDTOtoEntity(answerPostDTO);
+        // check content
+        assertEquals(answerPostDTO.getAnswer(), answer.getAnswer());
+        assertEquals(answerPostDTO.getTimeTaken(), answer.getTimeTaken());
+    }
+
+    @Test
+    public void testGetGameInfo_fromGameInfo_toGameInfoGetDTO_success() {
+        // create GameInfo
+        GameInfo gameInfo = new GameInfo();
+        gameInfo.setGameId(1L);
+        gameInfo.setCategory(CityCategory.ASIA);
+        gameInfo.setGameDate(new Date());
+        gameInfo.setGameRounds(2);
+        gameInfo.setPlayerNum(4);
+        // MAP -> Create gameInfoGetDTO
+        GameInfoGetDTO gameInfoGetDTO = DTOMapper.INSTANCE.convertEntityToGameInfoGetDTO(gameInfo);
+        // check content
+        assertEquals(gameInfo.getGameId(), gameInfoGetDTO.getGameId());
+        assertEquals(gameInfo.getCategory(), gameInfoGetDTO.getCategory());
+        assertEquals(gameInfo.getGameDate(), gameInfoGetDTO.getGameDate());
+        assertEquals(gameInfo.getGameRounds(), gameInfoGetDTO.getGameRounds());
+        assertEquals(gameInfo.getPlayerNum(), gameInfoGetDTO.getPlayerNum());
+    }
+
+    @Test
+    public void testGetGameHistory_fromGameHistory_toGameHistoryGetDTO_success() {
+        // create GameHistory
+        UserGameHistory gameHistory = new UserGameHistory(1L, 200);
+        // MAP -> Create GameHistoryGetDTO
+        GameHistoryGetDTO gameHistoryGetDTO = DTOMapper.INSTANCE.convertEntityToGameHistoryGetDTO(gameHistory);
+        // check content
+        assertEquals(gameHistory.getGameScore(), gameHistoryGetDTO.getGameScore());
     }
 }
