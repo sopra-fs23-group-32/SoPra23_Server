40ef211c234812cdb7fa907dbe62d308f64dba3d
awhoa <said.hajiabukar@uzh.ch>
2023-04-22 18:34:51 +0200

    adding game controller test

--- src/test/java/ch/uzh/ifi/hase/soprafs23/repository/GameInfoIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/repository/GameInfoIntegrationTest.java
@@ -1,46 +1,59 @@
-package ch.uzh.ifi.hase.soprafs23.repository;
+package ch.uzh.ifi.hase.soprafs23.controller;
 
-import ch.uzh.ifi.hase.soprafs23.constant.CityCategory;
-import ch.uzh.ifi.hase.soprafs23.entity.GameInfo;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.http.MediaType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
-import java.util.Date;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import ch.uzh.ifi.hase.soprafs23.constant.CityCategory;
+import ch.uzh.ifi.hase.soprafs23.entity.Game;
+import ch.uzh.ifi.hase.soprafs23.rest.dto.GamePostDTO;
+import ch.uzh.ifi.hase.soprafs23.rest.mapper.DTOMapper;
+import ch.uzh.ifi.hase.soprafs23.service.GameService;
+import ch.uzh.ifi.hase.soprafs23.service.UserService;
 
-@DataJpaTest
-public class GameInfoIntegrationTest {
+@WebMvcTest(GameController.class)
+
+public class GameControllerTest {
 
     @Autowired
-    private TestEntityManager entityManager;
+    private MockMvc mockMvc;
+    @MockBean
+    private GameService gameService;
+    @MockBean
+    private UserService userService;
 
-    @Autowired
-    private GameInfoRepository gameInfoRepository;
 
     @Test
-    public void findByGameId_success() {
-        // given
-        GameInfo gameInfo = new GameInfo();
-        gameInfo.setGameId(1L);
-        gameInfo.setCategory(CityCategory.ASIA);
-        gameInfo.setGameDate(new Date());
-        gameInfo.setGameRounds(2);
-        gameInfo.setPlayerNum(4);
+    public void testCreateGame() throws Exception {
+        GamePostDTO gamePostDTO = new GamePostDTO();
+        gamePostDTO.setCategory(CityCategory.EUROPE);
+        gamePostDTO.setTotalRounds(5);
+        gamePostDTO.setCountdownTime(10);
 
-        entityManager.persist(gameInfo);
-        entityManager.flush();
+        // Convert the GamePostDTO to a JSON string
+        ObjectMapper objectMapper = new ObjectMapper();
+        String gameJson = objectMapper.writeValueAsString(gamePostDTO);
 
-        // when
-        GameInfo found = gameInfoRepository.findByGameId(gameInfo.getGameId());
-        // then
-        assertNotNull(found.getGameId());
-        assertEquals(found.getCategory(), gameInfo.getCategory());
-        assertEquals(found.getGameDate(), gameInfo.getGameDate());
-        assertEquals(found.getGameRounds(), gameInfo.getGameRounds());
-        assertEquals(found.getPlayerNum(), found.getPlayerNum());
+    Game game= DTOMapper.INSTANCE.convertGamePostDTOtoEntity(gamePostDTO);
+        when(gameService.createGame(any(Game.class))).thenReturn(game);
+        MockHttpServletRequestBuilder postRequest = post("/games")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(gameJson);
+
+        // Perform the POST request
+        mockMvc.perform(postRequest)
+                .andExpect(status().isCreated());
     }
 }
