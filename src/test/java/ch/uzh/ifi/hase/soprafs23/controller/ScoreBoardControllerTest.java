c43111dc093198577e5df1d26a43a7a7fe9cb769
awhoa <said.hajiabukar@uzh.ch>
2023-05-22 13:20:14 +0200

    revert

--- src/test/java/ch/uzh/ifi/hase/soprafs23/controller/ScoreBoardControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/controller/ScoreBoardControllerTest.java
@@ -26,7 +26,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+//sfsf
 /**
  * ScoreBoardControllerTest
  * This is a WebMvcTest which allows to test the ScoreBoardController i.e. GET
